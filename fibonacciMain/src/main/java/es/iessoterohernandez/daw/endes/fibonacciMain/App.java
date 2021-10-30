d20a9d457adc8775b062c2cfaa8cbee84c0f911a
ruben <ruben@DESKTOP-AK8UJH5>
2021-10-30 20:13:02 +0200

    Segundo proyecto

---
+++ fibonacciMain/src/main/java/es/iessoterohernandez/daw/endes/fibonacciMain/App.java
@@ -0,0 +1,11 @@
+package es.iessoterohernandez.daw.endes.fibonacciMain;
+
+import es.iessoterohernandez.daw.endes.fibonacci.FibonacciIterativo;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        FibonacciIterativo.ejecutarFibonacci();
+    }
+}
